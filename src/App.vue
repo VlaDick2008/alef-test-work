352b7e2a1291a79776b5f42351e58308d6fdc02e
VlaDick2008 <sochukushat@yandex.ru>
2024-08-28 15:00:57 +0300

    Забубенил шапку сайта + подключил шрифт

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <header>
-    <img src="" alt="">
-    <div>
-      <RouterLink to="/form">Форма</RouterLink>
-      <RouterLink to="/preview">Превью</RouterLink>
+  <header class="flex items-center justify-center w-full p-10 border-b relative border-[#111111]/50">
+    <img width="250px" class="absolute left-0" src="/logo.svg" alt="logo">
+    <div class="flex gap-5 text-lg text-secondary">
+      <RouterLink class="hover:underline" to="/form">Форма</RouterLink>
+      <RouterLink class="hover:underline" to="/preview">Превью</RouterLink>
     </div>
   </header>
 
   <RouterView />
 </template>
+
+<style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
 	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontSize: {
+				"16-paragraph-2": "14px",
+				"16-paragraph-2-medium": "16px",
+				"13-paragraph-3": "13px",
+			},
+			textColor: {
+				primary: "#111111",
+				secondary: "rgba(17, 17, 17, 0.5)",
+				accent: "#01A7FD",
+			},
+		},
 	},
 	plugins: [],
 };
