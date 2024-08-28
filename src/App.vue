a949f5e0a8b6b373fc8aa331dcae0a56e99b0917
VlaDick2008 <sochukushat@yandex.ru>
2024-08-28 14:06:45 +0300

    Чуть не забыл на гитхабикус залить

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <header>
+    <img src="" alt="">
+    <div>
+      <RouterLink to="/form">Форма</RouterLink>
+      <RouterLink to="/preview">Превью</RouterLink>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
