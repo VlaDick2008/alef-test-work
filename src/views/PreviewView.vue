4a772d5b0b8d3d160778e3f6ecad658b6e60e48e
VlaDick2008 <sochukushat@yandex.ru>
2024-08-29 19:13:30 +0300

    Ещё одна попытка фикса

--- src/views/PreviewView.vue
+++ src/views/PreviewView.vue
@@ -1,25 +1,39 @@
 <script setup lang="ts">
 import ChildInfoBlock from '@/components/ChildInfoBlock.vue';
 import type { Data } from './FormView.vue';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
-const data = ref<Data>(JSON.parse(localStorage.getItem('data') ?? '{}'))
+const data = ref<Data | null>()
+
+function getJsonFromLocalStorage(key: string) {
+  const storedData = localStorage.getItem(key);
+  try {
+    return storedData ? JSON.parse(storedData) : null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+onMounted(() => {
+  data.value = getJsonFromLocalStorage('data')
+})
 </script>
 
 <template>
-  <main v-if="data.value" class="max-w-2xl mx-auto">
-    <div v-if="data.parentName !== ''">
+  <main v-if="data?.value" class="max-w-2xl mx-auto">
+    <div v-if="data?.parentName !== ''">
       <div class="mb-10">
         <h1 class="text-xl text-primary font-medium my-7">Персональные данные</h1>
         <div class="flex flex-col gap-2">
-          <p class="text-xl font-bold">{{ data.parentName }}, {{ data.parentAge }} лет</p>
+          <p class="text-xl font-bold">{{ data?.parentName }}, {{ data?.parentAge }} лет</p>
         </div>
       </div>
 
       <div>
-        <div v-if="data.amountOfKids.length > 0" class="flex gap-5 items-start flex-col">
+        <div v-if="data?.amountOfKids.length > 0" class="flex gap-5 items-start flex-col">
           <h1 class="text-xl text-primary font-medium mt-7">Дети</h1>
-          <div v-for="childrenBlock in data.amountOfKids" :key="childrenBlock.id">
+          <div v-for="childrenBlock in data?.amountOfKids" :key="childrenBlock.id">
             <ChildInfoBlock :name="childrenBlock.name" :age="childrenBlock.age" />
           </div>
         </div>
